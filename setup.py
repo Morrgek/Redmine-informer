f41dc14c6d311900e389046a2059a727323568ad
Yuliya Karpenko <karpenko@vigroup.ru>
2015-10-27 14:44:33 +0500

    first

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='test50',
+    version='1',
+    packages=[''],
+    url='http://dev50.local/main.py',
+    license='',
+    author='Yuliya',
+    author_email='',
+    description=''
+)
